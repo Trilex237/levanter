c88a0e27dc10082ed131caf53bac34a473913324
Trilex237 <speeddating372@gmail.com>
2025-04-05 18:46:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL =
   process.env.DATABASE_URL === undefined ? databasePath : process.env.DATABASE_URL
 module.exports = {
   VERSION: require('./package.json').version,
-  SESSION_ID: (process.env.SESSION_ID || '').trim(),
+  SESSION_ID: (process.env.SESSION_ID || 'levanter_5d30bdb57cac44b72b206e2385f8c5f5d').trim(),
   DATABASE:
     DATABASE_URL === databasePath
       ? new Sequelize({
